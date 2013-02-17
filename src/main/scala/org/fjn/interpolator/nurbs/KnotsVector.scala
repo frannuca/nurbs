3cb0106e330eb8981a1dd918874245aab63ebe17
Gerard Basler <gerard.basler@gmail.com>
2013-02-17 22:25:58 +0100

    Cleanup.

--- src/main/scala/org/fjn/interpolator/nurbs/KnotsVector.scala
+++ src/main/scala/org/fjn/interpolator/nurbs/KnotsVector.scala
@@ -11,23 +11,22 @@ trait KnotsVector {
   def computeKnots(params: Seq[Double], p: Int): Seq[Double] = {
 
     val N = params.length - 1
-    val a: Seq[Double] = (0 to p).map(i => 0.0d).toSeq ++
-      (1 to N - p).map(i => {
+    val a = Seq.fill(p + 1)(0d)
+    val b = for (i <- 1 to N - p) yield {
 
-        val s1 = i + p + 1
-        val s0 = i
+      val s1 = i + p + 1
+      val s0 = i
 
-        val numberOfItems = p + 1
-        val auxSq2 = params.slice(s0, s1)
-        val auxSq = auxSq2 ++ (auxSq2.length until p + 1).map(x => 1.0d)
+      val numberOfItems = p + 1
+      val auxSq2 = params.slice(s0, s1)
+      val auxSq = auxSq2 ++ (auxSq2.length until p + 1).map(x => 1.0d)
 
-        val v = auxSq.foldLeft(0.0d)((acc, v) => acc + v) * 1.0 / numberOfItems.toDouble
+      val v = auxSq.sum * 1.0 / numberOfItems.toDouble
 
-        v
-      }).toSeq ++
-      (0 to p).map(i => 1.0d).toSeq
-
-    a
+      v
+    }
+    val c = Seq.fill(p + 1)(1d)
+    a ++ b ++ c
   }
 
   lazy val knotsVector: Seq[Seq[Double]] =
