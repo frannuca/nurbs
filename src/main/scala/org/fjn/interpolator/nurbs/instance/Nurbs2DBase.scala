f21d02775a710faf5db0b9a3977005973d0f21d9
Gerard Basler <gerard.basler@gmail.com>
2013-02-23 18:29:04 +0100

    Readding lost commit

--- src/main/scala/org/fjn/interpolator/nurbs/instance/Nurbs2DBase.scala
+++ src/main/scala/org/fjn/interpolator/nurbs/instance/Nurbs2DBase.scala
@@ -3,7 +3,7 @@ package org.fjn.interpolator.nurbs.instance
 import org.fjn.interpolator.basis._
 import breeze.linalg.DenseMatrix
 import org.fjn.interpolator.nurbs.solver.Solver2D
-import collection.immutable.IndexedSeq
+import scala.math._
 
 trait Nurbs2DBase
     extends ControlPoint
@@ -17,7 +17,7 @@ trait Nurbs2DBase
 
   def apply(u: Double, v: Double): DenseMatrix[Double] = {
 
-    var sum = new DenseMatrix[Double](3, 1)
+    val sum = new DenseMatrix[Double](3, 1)
     for (i <- getBasisRange(0)(u)) {
       for (j <- getBasisRange(1)(v)) {
         val pAux = new DenseMatrix[Double](3, 1)
@@ -25,7 +25,7 @@ trait Nurbs2DBase
         pAux(1, 0) = pk(i)(j, 1)
         pAux(2, 0) = pk(i)(j, 2)
         val basis = (NBasis(i, basisOrderForCoord(0), 0)(u) * NBasis(j, basisOrderForCoord(1), 1)(v))
-        sum = sum + pAux * basis
+        sum += pAux * basis
       }
 
     }
@@ -46,6 +46,7 @@ trait Nurbs2DBase
   lazy val yReal: IndexedSeq[Double] = Real2TransformedAxis(numberOfPointsY, 1)
 
   def getNormalizedCoord(x: Double, nCoord: Int): Double = {
+    require(nCoord < 2, "Fast lookup only implemented up to 2 dimensions!")
 
     val (axis, nPoints) =
       if (nCoord == 0) {
@@ -131,29 +132,21 @@ trait Nurbs2DBase
    */
   def getBasisRange(nCoord: Int)(t: Double): Seq[Int] = {
 
-    //return 0 until  knotsVector(nCoord).length - basisOrder(nCoord)    -1
-
     val vector = knotsVector(nCoord)
-    val sz = vector.length - basisOrderForCoord(nCoord) - 1
 
-    var i = 0
-    var found: Boolean = false
-    var counter = 0
-    while (!found && counter < sz) {
-      if (t <= vector(counter)) {
-        i = counter
-        found = true
-      }
-      counter = counter + 1
+    // number of points that a basis function takes to the left / right
+    val numberOfPoints = basisOrderForCoord(nCoord) + 1
+
+    val i = vector.indexWhere(t <= _)
+
+    val resVector = if (i != -1) {
+      val leftIndex = max(0, i - numberOfPoints)
+      val rightIndex = min(i + numberOfPoints, vector.length - numberOfPoints)
+      leftIndex until rightIndex
+    } else {
+      vector.indices
     }
 
-    val resVector =
-      if (found) {
-        i - basisOrderForCoord(nCoord) - 1 to i + basisOrderForCoord(nCoord) + 1
-      } else
-        0 until vector.length
-
-    resVector.filter(c => c >= 0 && c < sz)
-
+    resVector
   }
 }
